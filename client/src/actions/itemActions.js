b3bf9d2d6fb231562b0e22ee406dacb0d49e478b
DanielChen1234 <dchen8492@yahoo.com>
2019-02-15 15:49:44 -0500

    crd working

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,15 +1,20 @@
-import {GET_ITEMS, ADD_ITEMS, DELETE_ITEMS} from './types'
+import axios from 'axios'
+import {GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING} from './types'
 
-export const getItems = () => ({
-    type: GET_ITEMS
-})
+export const getItems = () => (dispatch) => {
+    dispatch(setItemsLoading())
+    axios.get('/api/items').then(res => dispatch({type: GET_ITEMS, payload: res.data}))
+}
 
-export const deleteItem = (id) => ({
-    type: DELETE_ITEMS,
-    payload: id
-})
+export const addItem = (obj) => (dispatch) => {
+    axios.post('/api/items', obj).then(res => dispatch({type: ADD_ITEMS, payload: res.data}))
+}
 
-export const addItem = (obj) => ({
-    type: ADD_ITEMS,
-    payload: obj
+export const deleteItem = (id) => (dispatch) => {
+    axios.delete(`/api/items/${id}`).then(res => dispatch({type: DELETE_ITEMS, payload: id}))
+}
+
+
+export const setItemsLoading = () => ({
+    type: ITEMS_LOADING
 })
--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from 'reactstrap'
 import {connect} from 'react-redux'
 import {addItem} from '../actions/itemActions'
-import uuid from 'uuid'
 
 class ItemModel extends Component {
     constructor(){
@@ -27,7 +26,6 @@ class ItemModel extends Component {
         evt.preventDefault();
 
         const newItem = {
-            id: uuid(),
             name: this.state.name
         }
 
