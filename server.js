dfc2aedfc7e70b17f81592ff9094420cef2da8a4
DanielChen1234 <dchen8492@yahoo.com>
2019-02-16 13:07:22 -0500

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -21,6 +22,16 @@ mongoose
 
 //use routes
 app.use('/api/items', items)
+
+//Serve static assets if we're in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        //load index html anything that we get that isnt app.use('/api/items', items)
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // go to client. go to build. go to index html
+    })
+}
 
 const port = process.env.PORT || 5000; //for HEROKU or locally on port 5000.
 
